63b6a0336848c83228ca13db156bef30ad5d8e9e
svacaru99 <57227308+svacaru99@users.noreply.github.com>
2020-03-29 20:13:06 -0400

    Update dir_str_sample.py
    Added bash command line version as a comment

--- dir_str_sample.py
+++ dir_str_sample.py
@@ -6,3 +6,8 @@ for i in dirs:
     
 # Command Line Usage
 # python -c 'import os,sys;dirs=[ r for r,s,f in os.walk(".") if r != "."];[os.makedirs(os.path.join(sys.argv[1],i)) for i in dirs]' ~/new_destination
+#
+# Using MacOS bash
+# find . -type d -exec echo "'{}'" \; > dirs2.txt
+# cd to the target location and copy the dirs2.txt file there
+# xargs mkdir < dirs2.txt
